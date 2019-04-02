a8588718f037f3c4212bf993ba9e61b31b791f29
EtoiledeScauchy <44090250+EtoiledeScauchy@users.noreply.github.com>
2019-04-02 17:36:22 +0200

    work in progress - shadow models

---
+++ src/models/shadow_model.py
@@ -0,0 +1,15 @@
+import torch
+import torch.utils.data
+shadow_data = torch.utils.data.Dataset
+
+M = 7
+batch_size = 64
+
+shadow_dataset_size = len(shadow_data)/M # suppose que ça tombe juste
+
+unsplit_shadow_datasets = random_split(shadow_data, shadow_dataset_size)
+
+split_shadow_datasets = []
+for dataset in shadow_datasets:
+  learnable_shadow_dataset = random_split(dataset, batch_size)
+  split_shadow_datasets.append(learnable_shadow_dataset)
