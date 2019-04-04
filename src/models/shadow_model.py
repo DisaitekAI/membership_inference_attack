1526770b7d93d85b8a47e538d10553c758fcc4da
EtoiledeScauchy <44090250+EtoiledeScauchy@users.noreply.github.com>
2019-04-04 04:50:32 +0200

    work in progress

--- src/models/shadow_model.py
+++ src/models/shadow_model.py
@@ -1,23 +1,28 @@
 import torch
 import torch.utils.data
-import mnist_model.py
+import mnist_model
+import dataset_generator
 
-shadow_data = torch.utils.data.Dataset
-current_model = Mnist_model
-
-M = 7
-batch_size = 64
-
-shadow_dataset_size = len(shadow_data) // M # suppose que ça tombe juste
-
-unsplit_shadow_datasets = random_split(shadow_data, shadow_dataset_size)
-
-split_shadow_datasets = []
-for dataset in unsplit_shadow_datasets:
-  learnable_shadow_dataset = random_split(dataset, batch_size)
-  split_shadow_datasets.append(learnable_shadow_dataset)
-
-shadow_models = []
-for split_dataset in split_shadow_datasets:
-  shadow_model = current_model(split_dataset) # pas sûr que ce soit la bonne syntaxe
-  shadow_models.append(shadow_model)
+class swarm_shadow_model:
+  def __init__(self, dataset, model, M):
+    self.data = dataset
+    self.model = model
+    self.swarm_number = M
+    self.shadow_size = len(dataset) // M
+  def split(self):
+    return random_split(self.data, self.shadow_size)
+  def get_loader(self, batch_size):
+    return [torch.utils.data.DataLoader(shadow_data, batch_size = batch_size, shuffle = True, **cuda_args) for shadow_data in split(self)]
+  def training(self, batch_size, epoch_number):
+    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+    device = torch.device("cuda" if use_cuda else "cpu")
+    shadow_models = [self.model for i in range(self.swarm_number)]
+    loaders = get_loader(self, batch_size)
+    index = 0
+    for shadow in shadow_models:
+      for epoch in range(epoch_number):
+          train(shadow, device, loaders[index], optimizer, epoch)
+      index += 1
+    return shadow_models
+  def generate_attack_data(self, batch_size, epoch_number):
+    
