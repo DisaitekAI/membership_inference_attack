4867a65f9eaa94d2b0945d6303eabcf822f47d24
Antoine de Scorraille <antoine.de.scorraille@gmail.com>
2019-04-18 14:26:31 +0200

    normalization unittest of the test code for statistics (WIP)

--- src/tests/test_statistics.py
+++ src/tests/test_statistics.py
@@ -1,11 +1,95 @@
+import pathlib, sys
+
+home_path = pathlib.Path('.').resolve()
+while home_path.name != 'membership_inference_attack':
+    home_path = home_path.parent
+  
+#data_src_path = home_path/'src'/'data'
+utils_path = home_path/'src'/'utils'
+  
+# add ../utils/ into the path
+if utils_path.as_posix() not in sys.path:
+    sys.path.insert(0, utils_path.as_posix())
+
 from sklearn.metrics import confusion_matrix, classification_report, balanced_accuracy_score, \
 roc_auc_score, classification_report, roc_curve
 import matplotlib.pyplot as plt
 from matplotlib import ticker
 import numpy as np
-import ../utils/statistics
-#import statistics
+import unittest
+#import ../src/utils/statistics
+from statistics import Statistics
 
+
+class TestStatistics(unittest.TestCase):
+    def test_new_exp1(self,st):
+        st.new_experiment("loremipsum", {"batch_size": 64, "lambda": 17})
+        n = "{} ".format(len(st.list_experiment) - 1) + "loremipsum"
+        self.assertEqual(st.current_experiment_name, n)
+        self.assertEqual(st.list_experiment[-1], n)
+        self.assertEqual(st.param[n], {"batch_size": 64, "lambda": 17})
+        self.assertEqual(st.balanced_accuracy[n], [])
+        self.assertEqual(st.roc_area[n], [])
+        self.assertEqual(st.batch_count[n], 0)
+        self.assertEqual(st.epoch_count[n], 0)
+    
+    def test_new_ep(self,st):
+        past_epoch_count = st.epoch_count[st.current_experiment_name]
+        st.new_epoch()
+        self.assertEqual(st.epoch_count[st.current_experiment_name], past_epoch_count + 1)
+        
+    def test_new_add_b1(self, st):
+        pred = [0,0,0,0,1]
+        real = [1,0,0,0,1]
+        past_number_batch = st.batch_count[st.current_experiment_name]
+        past_y_pred = list(st.y_pred)
+        past_y_real = list(st.y_true)
+        st.add_batch_results(pred,real)
+        self.assertGreater(st.epoch_count[st.current_experiment_name], 0)
+        if st.epoch_count[st.current_experiment_name] == 1:
+            self.assertEqual(st.batch_count[st.current_experiment_name], past_number_batch + 1)
+        else:
+            self.assertEqual(st.batch_count[st.current_experiment_name], past_number_batch)
+        self.assertEqual(st.y_pred, past_y_pred + pred)
+        self.assertEqual(st.y_true, past_y_real + real)
+        #print(len(st.balanced_accuracy[st.current_experiment_name]), st.epoch_count[st.current_experiment_name], st.batch_count[st.current_experiment_name])
+        #assert len(st.balanced_accuracy[st.current_experiment_name]) == (st.epoch_count[st.current_experiment_name] - 1) * st.batch_count[st.current_experiment_name] + st.batch_count[st.current_experiment_name]
+        #assert len(st.roc_area[st.current_experiment_name]) == (st.epoch_count[st.current_experiment_name] - 1) * st.batch_count[st.current_experiment_name] + st.batch_count[st.current_experiment_name]
+        self.assertEqual(st.balanced_accuracy[st.current_experiment_name][-1], 0.875)
+        self.assertEqual(st.roc_area[st.current_experiment_name][-1], 0.875)
+        
+    def test_new_exp2(self,st):
+        st.new_experiment("alive", {"batch_size": 64, "beta": 17})
+        n = "{} ".format(len(st.list_experiment) - 1) + "alive"
+        self.assertEqual(st.current_experiment_name, n)
+        self.assertEqual(st.list_experiment[-1], n)
+        self.assertEqual(st.param[n], {"batch_size": 64, "lambda": 17})
+        self.assertEqual(st.balanced_accuracy[n], [])
+        self.assertEqual(st.roc_area[n], [])
+        self.assertEqual(st.batch_count[n], 0)
+        self.assertEqual(st.epoch_count[n], 0)
+        
+    def test_new_add_b2(self, st):
+        pred = [0,0,0,0,1,0,1,1,1]
+        real = [1,0,0,0,1,0,1,0,1]
+        past_number_batch = st.batch_count[st.current_experiment_name]
+        past_y_pred = list(st.y_pred)
+        past_y_real = list(st.y_true)
+        st.add_batch_results(pred,real)
+        self.assertGreater(st.epoch_count[st.current_experiment_name], 0)
+        if st.epoch_count[st.current_experiment_name] == 1:
+            self.assertEqual(st.batch_count[st.current_experiment_name], past_number_batch + 1)
+        else:
+            self.assertEqual(st.batch_count[st.current_experiment_name], past_number_batch)
+        self.assertEqual(st.y_pred, past_y_pred + pred)
+        self.assertEqual(st.y_true, past_y_real + real)
+        #print(len(st.balanced_accuracy[st.current_experiment_name]), st.epoch_count[st.current_experiment_name], st.batch_count[st.current_experiment_name])
+        #assert len(st.balanced_accuracy[st.current_experiment_name]) == (st.epoch_count[st.current_experiment_name] - 1) * st.batch_count[st.current_experiment_name] + st.batch_count[st.current_experiment_name]
+        #assert len(st.roc_area[st.current_experiment_name]) == (st.epoch_count[st.current_experiment_name] - 1) * st.batch_count[st.current_experiment_name] + st.batch_count[st.current_experiment_name]
+        self.assertEqual(st.balanced_accuracy[st.current_experiment_name][-1], 0.775)
+        self.assertEqual(st.roc_area[st.current_experiment_name][-1], 0.7750000000000001)
+
+        
 def new_exp1(st):
     st.new_experiment("loremipsum", {"batch_size": 64, "lambda": 17})
     n = "{} ".format(len(st.list_experiment) - 1) + "loremipsum"
@@ -87,4 +171,6 @@ def main():
             add_b2(stat)
 
 if __name__ == '__main__':
-    main()
+    #main()
+    print("azda")
+    unittest.main()
--- src/utils/statistics.py
+++ src/utils/statistics.py
@@ -5,92 +5,91 @@ from matplotlib import ticker
 import numpy as np
 
 class Statistics:
-  def __init__(self):
-    self.y_pred = []
-    self.y_true = []
-    self.balanced_accuracy = dict()
-    self.list_experiment = []
-    
-    self.current_experiment_name = ""
-    self.list_experiment = []
-    self.param = dict()
-    self.confusion_matrix = [[0,0],[0,0]]
-    self.roc_area = dict()
-    self.report = ""
-    self.batch_count = dict()
-    self.epoch_count = dict()
+    def __init__(self):
+        self.y_pred = []
+        self.y_true = []
+        self.balanced_accuracy = dict()
+        self.list_experiment = []
+
+        self.current_experiment_name = ""
+        self.list_experiment = []
+        self.param = dict()
+        self.confusion_matrix = [[0,0],[0,0]]
+        self.roc_area = dict()
+        self.report = ""
+        self.batch_count = dict()
+        self.epoch_count = dict()
 
 
     
     ############# Entrées #############
     
-  def new_experiment(self, name, parameters):
-    self.current_experiment_name = "{} ".format(len(self.list_experiment)) + name
-    self.list_experiment.append(self.current_experiment_name)
-    self.param[self.current_experiment_name] = parameters
-    
-    self.balanced_accuracy[self.current_experiment_name] = []
-    self.roc_area[self.current_experiment_name] = []
-    self.batch_count[self.current_experiment_name] = 0
-    self.epoch_count[self.current_experiment_name] = 0
-    self.y_pred = []
-    self.y_true = []
+    def new_experiment(self, name, parameters):
+        self.current_experiment_name = "{} ".format(len(self.list_experiment)) + name
+        self.list_experiment.append(self.current_experiment_name)
+        self.param[self.current_experiment_name] = parameters
+
+        self.balanced_accuracy[self.current_experiment_name] = []
+        self.roc_area[self.current_experiment_name] = []
+        self.batch_count[self.current_experiment_name] = 0
+        self.epoch_count[self.current_experiment_name] = 0
+        self.y_pred = []
+        self.y_true = []
 
     
-  def new_epoch(self):
-    self.epoch_count[self.current_experiment_name] += 1
+    def new_epoch(self):
+        self.epoch_count[self.current_experiment_name] += 1
         
-  def add_batch_results(self, batch_pred, batch_true):
-    if self.epoch_count[self.current_experiment_name] == 1:
-        self.batch_count[self.current_experiment_name] += 1
-    self.y_pred.extend(batch_pred)
-    self.y_true.extend(batch_true)
-    new_accuracy = balanced_accuracy_score(self.y_pred, self.y_true)
-    self.balanced_accuracy[self.current_experiment_name].append(new_accuracy)
-    self.roc_area[self.current_experiment_name].append(roc_auc_score(self.y_pred, self.y_true))
+    def add_batch_results(self, batch_pred, batch_true):
+        if self.epoch_count[self.current_experiment_name] == 1:
+            self.batch_count[self.current_experiment_name] += 1
+        self.y_pred.extend(batch_pred)
+        self.y_true.extend(batch_true)
+        new_accuracy = balanced_accuracy_score(self.y_pred, self.y_true)
+        self.balanced_accuracy[self.current_experiment_name].append(new_accuracy)
+        self.roc_area[self.current_experiment_name].append(roc_auc_score(self.y_pred, self.y_true))
     
-  def end_epoch(self):
-    self.confusion_matrix = confusion_matrix(self.y_pred, self.y_true)
-    self.report = classification_report(self.y_pred, self.y_true)
-    self.y_pred = []
-    self.y_true = []
-    self.iteration = 0
+    def end_epoch(self):
+        self.confusion_matrix = confusion_matrix(self.y_pred, self.y_true)
+        self.report = classification_report(self.y_pred, self.y_true)
+        self.y_pred = []
+        self.y_true = []
+        self.iteration = 0
 
     ############# Sorties #############
 
+    def save(self, file_path): # à appeler à la toute fin
+        return
     
-  def save(self, file_path): # à appeler à la toute fin
-    return
-    
-  def print_results(self): # à appeler à chaque fin d'epoch
-    self.end_epoch()
-    print("Balanced accuracy score: {}".format(self.balanced_accuracy[self.current_experiment_name][-1]))
-    print("Confusion matrix:\n {}".format(self.confusion_matrix))
-    print("Curve ROC area: {}".format(self.roc_area[self.current_experiment_name][-1]))
-    print(self.report)
+    def print_results(self): # à appeler à chaque fin d'epoch
+        self.end_epoch()
+        print("Balanced accuracy score: {}".format(self.balanced_accuracy[self.current_experiment_name][-1]))
+        print("Confusion matrix:\n {}".format(self.confusion_matrix))
+        print("Curve ROC area: {}".format(self.roc_area[self.current_experiment_name][-1]))
+        print(self.report)
     
     
-  def plot(self):
-    print(self.batch_count)
-    print(self.epoch_count)
-    fig, ax1 = plt.subplots(1,1,figsize=(5,3),dpi=100)
-    ax1.plot(self.balanced_accuracy[self.current_experiment_name], )
-    #ax1.set_xlim((self.balanced_accuracy[self.current_experiment_name][0],self.balanced_accuracy[self.current_experiment_name][-1]))
-    print(self.balanced_accuracy[self.current_experiment_name])
-    xticklabs = ["{}".format(i) for i in range(self.epoch_count[self.current_experiment_name] * self.batch_count[self.current_experiment_name])]
-    for i in range(self.epoch_count[self.current_experiment_name]):
-        xticklabs[i * self.batch_count[self.current_experiment_name] - 1] = "Epoch {}".format(i) #r'$\textcolor{{red}}{{Epoch {}}}$'
-    xticklabs[-1] = "Epoch {}".format(self.epoch_count[self.current_experiment_name])
-    ax1.set_xticklabels(xticklabs)
-    print(xticklabs)
-    ax1.xaxis.set_minor_locator(MultipleLocator)
-    #for i in range(self.epoch_count):
-         #, data=(np.array([j for j in range(3)]),np.array([j for j in range(3)])
-        #plt.title('Epoch {}'.format(self.iteration))
-        #plt.axvline(x=i*self.epoch_count,color='red')
-    #plt.show()
-    fig.tight_layout()
-    return
+    def plot(self):
+        print(self.batch_count)
+        print(self.epoch_count)
+        fig, ax1 = plt.subplots(1,1,figsize=(5,3),dpi=100)
+        ax1.plot(self.balanced_accuracy[self.current_experiment_name], )
+        #ax1.set_xlim((self.balanced_accuracy[self.current_experiment_name][0],self.balanced_accuracy[self.current_experiment_name][-1]))
+        print(self.balanced_accuracy[self.current_experiment_name])
+        xticklabs = ["{}".format(i) for i in range(self.epoch_count[self.current_experiment_name] * self.batch_count[self.current_experiment_name])]
+        for i in range(self.epoch_count[self.current_experiment_name]):
+            xticklabs[i * self.batch_count[self.current_experiment_name] - 1] = "Epoch {}".format(i) #r'$\textcolor{{red}}{{Epoch {}}}$'
+        xticklabs[-1] = "Epoch {}".format(self.epoch_count[self.current_experiment_name])
+        ax1.set_xticklabels(xticklabs)
+        print(xticklabs)
+        ax1.xaxis.set_minor_locator(MultipleLocator)
+        #for i in range(self.epoch_count):
+             #, data=(np.array([j for j in range(3)]),np.array([j for j in range(3)])
+            #plt.title('Epoch {}'.format(self.iteration))
+            #plt.axvline(x=i*self.epoch_count,color='red')
+        #plt.show()
+        fig.tight_layout()
+        return 
     
 # ~ https://scikit-learn.org/stable/modules/generated/sklearn.metrics.confusion_matrix.html
 # ~ https://scikit-learn.org/stable/modules/generated/sklearn.metrics.accuracy_score.html
