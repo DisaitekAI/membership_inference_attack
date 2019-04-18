d0c2feb92eae6cc2fceb68aee3eb972316ee34ec
Antoine de Scorraille <antoine.de.scorraille@gmail.com>
2019-04-18 16:33:03 +0200

    correction of file do_experiments with the master version (WIP)

--- src/models/do_experiments.py
+++ src/models/do_experiments.py
@@ -18,28 +18,29 @@ if utils_path.as_posix() not in sys.path:
   sys.path.insert(0, utils_path.as_posix())
   
 from experiment import experiment
-from utils_modules import Flatten
+from utils_modules import Flatten, Print
 from collections import OrderedDict
 from statistics import Statistics
 import torch.nn as nn
   
-def main():
+def main():  
   experiment_stats = Statistics()
   
   # default regularized model
   experiment(academic_dataset       = 'mnist', 
-             target_model_path      = (models_path/'mnist_model.pt').as_posix(),
-             mia_model_path         = (models_path/'mia_model_default.pt').as_posix(),
+             target_model_path      = (models_path/'mnist_model_default.pt').as_posix(),
+             mia_model_path         = (models_path/'mia_model_default').as_posix(),
              shadow_model_base_path = (models_path/'shadows'/'shadow_default').as_posix(),
-             mia_train_dataset_path = (data_path/'mia_train_dataset_default.pt').as_posix(),
-             mia_test_dataset_path  = (data_path/'mia_test_dataset_default.pt').as_posix(),
+             mia_train_dataset_path = (data_path/'mia_train_dataset_default').as_posix(),
+             mia_test_dataset_path  = (data_path/'mia_test_dataset_default').as_posix(),
+             class_number           = 10,
              stats                  = experiment_stats,
-	     experiment_name          = "basic_mnist")
+             experiment_name          = "regularized_mnist")
   
   # without regularization
   experiment(academic_dataset    = 'mnist', 
-             target_model_path   = (models_path/'mnist_model.pt').as_posix(),
-             mia_model_path      = (models_path/'mia_model_exp1.pt').as_posix(),
+             target_model_path   = (models_path/'mnist_model_exp1.pt').as_posix(),
+             mia_model_path      = (models_path/'mia_model_exp1').as_posix(),
              custom_target_model = OrderedDict([
                ('conv1'       , nn.Conv2d(1, 10, 3, 1)),
                ('relu1'       , nn.ReLU()),
@@ -48,22 +49,23 @@ def main():
                ('relu2'       , nn.ReLU()),
                ('maxpool2'    , nn.MaxPool2d(2, 2)),
                ('to1d'        , Flatten()),
-               ('dense1'      , nn.Linear(11*11*10, 500)),
+               ('dense1'      , nn.Linear(5*5*10, 500)),
                ('tanh'        , nn.Tanh()),
                ('dense2'      , nn.Linear(500, 10)),
                ('logsoftmax'  , nn.LogSoftmax(dim=1))
              ]),
              shadow_number            = 50,
              shadow_model_base_path   = (models_path/'shadows'/'shadow_exp1').as_posix(),
-             mia_train_dataset_path   = (data_path/'mia_train_dataset_exp1.pt').as_posix(),
-             mia_test_dataset_path    = (data_path/'mia_test_dataset_exp1.pt').as_posix(),
+             mia_train_dataset_path   = (data_path/'mia_train_dataset_exp1').as_posix(),
+             mia_test_dataset_path    = (data_path/'mia_test_dataset_exp1').as_posix(),
+             class_number             = 10,
              stats                    = experiment_stats,
-	     experiment_name          = "regularized_mnist")
+             experiment_name          = "basic_mnist")
   
   # with dropout regularization
   experiment(academic_dataset    = 'mnist', 
-             target_model_path   = (models_path/'mnist_model.pt').as_posix(),
-             mia_model_path      = (models_path/'mia_model_exp2.pt').as_posix(),
+             target_model_path   = (models_path/'mnist_model_exp2.pt').as_posix(),
+             mia_model_path      = (models_path/'mia_model_exp2').as_posix(),
              custom_target_model = OrderedDict([
                ('conv1'       , nn.Conv2d(1, 10, 3, 1)),
                ('relu1'       , nn.ReLU()),
@@ -74,7 +76,7 @@ def main():
                ('maxpool2'    , nn.MaxPool2d(2, 2)),
                ('dropout2'    , nn.Dropout(p = 0.5)),
                ('to1d'        , Flatten()),
-               ('dense1'      , nn.Linear(11*11*10, 500)),
+               ('dense1'      , nn.Linear(5*5*10, 500)),
                ('tanh'        , nn.Tanh()),
                ('dropout3'    , nn.Dropout(p = 0.5)),
                ('dense2'      , nn.Linear(500, 10)),
@@ -82,10 +84,11 @@ def main():
              ]),
              shadow_number            = 50,
              shadow_model_base_path   = (models_path/'shadows'/'shadow_exp2').as_posix(),
-             mia_train_dataset_path   = (data_path/'mia_train_dataset_exp2.pt').as_posix(),
-             mia_test_dataset_path    = (data_path/'mia_test_dataset_exp2.pt').as_posix(),
+             mia_train_dataset_path   = (data_path/'mia_train_dataset_exp2').as_posix(),
+             mia_test_dataset_path    = (data_path/'mia_test_dataset_exp2').as_posix(),
+             class_number             = 10,
              stats                    = experiment_stats,
-	     experiment_name          = "dropout_regularized_mnist")
+             experiment_name          = "dropout_regularized_mnist")
              
   # ~ experiment_stats.print_results()
   # ~ experiment_stats.save(fila_path)
