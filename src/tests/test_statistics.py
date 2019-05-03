b6492cedf83603ac93f69441b27313dc2047ec4c
Antoine de Scorraille <antoine.de.scorraille@gmail.com>
2019-05-03 15:45:22 +0200

    unit tests of statistics class (WIP)

--- src/tests/test_statistics.py
+++ src/tests/test_statistics.py
@@ -4,7 +4,6 @@ home_path = pathlib.Path('.').resolve()
 while home_path.name != 'membership_inference_attack':
     home_path = home_path.parent
   
-#data_src_path = home_path/'src'/'data'
 utils_path = home_path/'src'/'utils'
   
 # add ../utils/ into the path
@@ -17,169 +16,166 @@ import matplotlib.pyplot as plt
 from matplotlib import ticker
 import numpy as np
 import unittest
-#import ../src/utils/statistics
+import time
 from statistics import Statistics
 
 
 class TestStatistics(unittest.TestCase):
-    def test_new_exp1(self):
-        st = Statistics()
-        st.new_experiment("loremipsum", {"batch_size": 64, "lambda": 17})
-        n = "{} ".format(len(st.list_experiment) - 1) + "loremipsum"
-        self.assertEqual(st.current_experiment_name, n)
-        self.assertEqual(st.list_experiment[-1], n)
-        self.assertEqual(st.param[n], {"batch_size": 64, "lambda": 17})
-        self.assertEqual(st.balanced_accuracy[n], [])
-        self.assertEqual(st.roc_area[n], [])
-        self.assertEqual(st.batch_count[n], 0)
-        self.assertEqual(st.epoch_count[n], 0)
-    
-    def test_new_ep(self):
-        st = Statistics()
-        st.new_experiment("loremipsum", {"batch_size": 64, "lambda": 17})
-        past_epoch_count = st.epoch_count[st.current_experiment_name]
-        st.new_epoch()
-        self.assertEqual(st.epoch_count[st.current_experiment_name], past_epoch_count + 1)
-        
-    def test_new_add_b1(self):
-        st = Statistics()
-        st.new_experiment("loremipsum", {"batch_size": 64, "lambda": 17})
-        st.new_epoch()
-        pred = [0,0,0,0,1]
-        real = [1,0,0,0,1]
-        past_number_batch = st.batch_count[st.current_experiment_name]
-        past_y_pred = list(st.y_pred)
-        past_y_real = list(st.y_true)
-        st.add_batch_results(pred,real)
-        self.assertGreater(st.epoch_count[st.current_experiment_name], 0)
-        if st.epoch_count[st.current_experiment_name] == 1:
-            self.assertEqual(st.batch_count[st.current_experiment_name], past_number_batch + 1)
-        else:
-            self.assertEqual(st.batch_count[st.current_experiment_name], past_number_batch)
-        self.assertEqual(st.y_pred, past_y_pred + pred)
-        self.assertEqual(st.y_true, past_y_real + real)
-        #print(len(st.balanced_accuracy[st.current_experiment_name]), st.epoch_count[st.current_experiment_name], st.batch_count[st.current_experiment_name])
-        #assert len(st.balanced_accuracy[st.current_experiment_name]) == (st.epoch_count[st.current_experiment_name] - 1) * st.batch_count[st.current_experiment_name] + st.batch_count[st.current_experiment_name]
-        #assert len(st.roc_area[st.current_experiment_name]) == (st.epoch_count[st.current_experiment_name] - 1) * st.batch_count[st.current_experiment_name] + st.batch_count[st.current_experiment_name]
-        self.assertEqual(st.balanced_accuracy[st.current_experiment_name][-1], 0.875)
-        self.assertEqual(st.roc_area[st.current_experiment_name][-1], 0.875)
-        
-    def test_new_exp2(self):
-        st = Statistics()
-        st.new_experiment("alive", {"batch_size": 64, "beta": 17})
-        n = "{} ".format(len(st.list_experiment) - 1) + "alive"
-        self.assertEqual(st.current_experiment_name, n)
-        self.assertEqual(st.list_experiment[-1], n)
-        self.assertEqual(st.param[n], {"batch_size": 64, "beta": 17})
-        self.assertEqual(st.balanced_accuracy[n], [])
-        self.assertEqual(st.roc_area[n], [])
-        self.assertEqual(st.batch_count[n], 0)
-        self.assertEqual(st.epoch_count[n], 0)
-        
-    def test_new_add_b2(self):
-        st = Statistics()
-        st.new_experiment("alive", {"batch_size": 64, "beta": 17})
-        st.new_epoch()
-        pred = [0,0,0,0,1,0,1,1,1]
-        real = [1,0,0,0,1,0,1,0,1]
-        past_number_batch = st.batch_count[st.current_experiment_name]
-        past_y_pred = list(st.y_pred)
-        past_y_real = list(st.y_true)
-        st.add_batch_results(pred,real)
-        self.assertGreater(st.epoch_count[st.current_experiment_name], 0)
-        if st.epoch_count[st.current_experiment_name] == 1:
-            self.assertEqual(st.batch_count[st.current_experiment_name], past_number_batch + 1)
-        else:
-            self.assertEqual(st.batch_count[st.current_experiment_name], past_number_batch)
-        self.assertEqual(st.y_pred, past_y_pred + pred)
-        self.assertEqual(st.y_true, past_y_real + real)
-        #print(len(st.balanced_accuracy[st.current_experiment_name]), st.epoch_count[st.current_experiment_name], st.batch_count[st.current_experiment_name])
-        #assert len(st.balanced_accuracy[st.current_experiment_name]) == (st.epoch_count[st.current_experiment_name] - 1) * st.batch_count[st.current_experiment_name] + st.batch_count[st.current_experiment_name]
-        #assert len(st.roc_area[st.current_experiment_name]) == (st.epoch_count[st.current_experiment_name] - 1) * st.batch_count[st.current_experiment_name] + st.batch_count[st.current_experiment_name]
-        self.assertEqual(st.balanced_accuracy[st.current_experiment_name][-1], 0.775)
-        self.assertEqual(st.roc_area[st.current_experiment_name][-1], 0.7750000000000001)
 
-        
-def new_exp1(st):
-    st.new_experiment("loremipsum", {"batch_size": 64, "lambda": 17})
-    n = "{} ".format(len(st.list_experiment) - 1) + "loremipsum"
-    assert st.current_experiment_name == n
-    assert st.list_experiment[-1] == n
-    assert st.param[n] == {"batch_size": 64, "lambda": 17}
-    assert st.balanced_accuracy[n] == []
-    assert st.roc_area[n] == []
-    assert st.batch_count[n] == 0
-    assert st.epoch_count[n] == 0
-    
-def new_ep(st):
-    past_epoch_count = st.epoch_count[st.current_experiment_name]
-    st.new_epoch()
-    assert st.epoch_count[st.current_experiment_name] == past_epoch_count + 1
-        
-def add_b1(st):
-    pred = [0,0,0,0,1]
-    real = [1,0,0,0,1]
-    past_number_batch = st.batch_count[st.current_experiment_name]
-    past_y_pred = list(st.y_pred)
-    past_y_real = list(st.y_true)
-    st.add_batch_results(pred,real)
-    assert st.epoch_count[st.current_experiment_name] > 0
-    if st.epoch_count[st.current_experiment_name] == 1:
-        assert st.batch_count[st.current_experiment_name] == past_number_batch + 1
-    else:
-        assert st.batch_count[st.current_experiment_name] == past_number_batch
-    assert st.y_pred == past_y_pred + pred
-    assert st.y_true == past_y_real + real
-    #print(len(st.balanced_accuracy[st.current_experiment_name]), st.epoch_count[st.current_experiment_name], st.batch_count[st.current_experiment_name])
-    #assert len(st.balanced_accuracy[st.current_experiment_name]) == (st.epoch_count[st.current_experiment_name] - 1) * st.batch_count[st.current_experiment_name] + st.batch_count[st.current_experiment_name]
-    #assert len(st.roc_area[st.current_experiment_name]) == (st.epoch_count[st.current_experiment_name] - 1) * st.batch_count[st.current_experiment_name] + st.batch_count[st.current_experiment_name]
-    assert st.balanced_accuracy[st.current_experiment_name][-1] == 0.875
-    assert st.roc_area[st.current_experiment_name][-1] == 0.875
-    
-def new_exp2(st):
-    st.new_experiment("alive", {"batch_size": 64, "beta": 17})
-    n = "{} ".format(len(st.list_experiment) - 1) + "alive"
-    assert st.current_experiment_name == n
-    assert st.list_experiment[-1] == n
-    assert st.param[n] == {"batch_size": 64, "beta": 17}
-    assert st.balanced_accuracy[n] == []
-    assert st.roc_area[n] == []
-    assert st.batch_count[n] == 0
-    assert st.epoch_count[n] == 0
+  def test_new_experiment(self):
+    st = Statistics()
+    st.new_experiment('loremipsum', {'batch_size': 64, 'lambda': 17})
+    self.assertEqual(st.y_pred, [])
+    self.assertEqual(st.y_true, [])
+    self.assertEqual(st.exp, 
+        [ { 'name': 'loremipsum', 'param': {'batch_size': 64, 'lambda': 17},
+           'model_training': [], 'mia_stats' : { 'mia_train_in_distribution' : [],
+                                                 'mia_train_out_distribution': [],
+                                                 'mia_test_in_distribution'  : [],
+                                                 'mia_test_out_distribution' : [] } } ] )
+    self.assertIsNone(st.resume)
 
+  def test_new_train(self):
+    st = Statistics()
+    st.new_experiment('loremipsum', {'batch_size': 64, 'lambda': 17})
+    test_start = time.time()
+    st.new_train('martel', 'hell')
+    self.assertEqual(st.y_pred, [])
+    self.assertEqual(st.y_true, [])
+    self.assertEqual(len(st.exp), 1)
+    self.assertEqual(st.exp[0]['name'], 'loremipsum')
+    self.assertEqual(st.exp[0]['param'], {'batch_size': 64, 'lambda': 17})
+    self.assertEqual(st.exp[0]['model_training'][0]['name'], 'martel')
+    self.assertEqual(st.exp[0]['model_training'][0]['label'], 'hell')
+    self.assertEqual(st.exp[0]['model_training'][0]['loss'], [])
+    self.assertEqual(len(st.exp[0]['model_training'][0]['time']), 1)
+    self.assertAlmostEqual(st.exp[0]['model_training'][0]['time'][0], test_start, places = 4)
+    self.assertEqual(st.exp[0]['model_training'][0]['measures'], { 'balanced_accuracy': [], 
+                                                                'roc_area'         : [], 
+                                                                'report'           : '' } )
+    self.assertEqual(st.exp[0]['mia_stats'], { 'mia_train_in_distribution' : [],
+                                               'mia_train_out_distribution': [],
+                                               'mia_test_in_distribution'  : [],
+                                               'mia_test_out_distribution' : [] })
+
+    def test_new_epoch(self):
+      st = Statistics()
+      st.new_experiment('loremipsum', {'batch_size': 64, 'lambda': 17})
+      test_start = time.time()
+      st.new_train('martel', 'hell')
+      self.assertEqual(st.y_pred, [])
+      self.assertEqual(st.y_true, [])
+      self.assertEqual(len(st.exp), 1)
+      self.assertEqual(st.exp[0]['name'], 'loremipsum')
+      self.assertEqual(st.exp[0]['param'], {'batch_size': 64, 'lambda': 17})
+      self.assertEqual(st.exp[0]['model_training'][0]['name'], 'martel')
+      self.assertEqual(st.exp[0]['model_training'][0]['label'], 'hell')
+      self.assertEqual(st.exp[0]['model_training'][0]['loss'], [ [] ])
+      self.assertEqual(len(st.exp[0]['model_training'][0]['time']), 1)
+      self.assertAlmostEqual(st.exp[0]['model_training'][0]['time'][0], test_start, places = 4)
+      self.assertEqual(st.exp[0]['model_training'][0]['measures'], { 'balanced_accuracy': [], 
+                                                                  'roc_area'         : [], 
+                                                                  'report'           : '' } )
+      self.assertEqual(st.exp[0]['mia_stats'], { 'mia_train_in_distribution' : [],
+                                                 'mia_train_out_distribution': [],
+                                                 'mia_test_in_distribution'  : [],
+                                                 'mia_test_out_distribution' : [] } )
+    def test_new_batch(self):
+      st = Statistics()
+      st.new_experiment('loremipsum', {'batch_size': 64, 'lambda': 17})
+      test_start = time.time()
+      st.new_train('martel', 'hell')
+      pred = [0,0,0,0,1]
+      real = [1,0,0,0,1]
+      st.new_batch(pred,real)
+      self.assertEqual(st.y_pred, pred)
+      self.assertEqual(st.y_true, real)
+      self.assertEqual(len(st.exp), 1)
+      self.assertEqual(st.exp[0]['name'], 'loremipsum')
+      self.assertEqual(st.exp[0]['param'], {'batch_size': 64, 'lambda': 17})
+      self.assertEqual(st.exp[0]['model_training'][0]['name'], 'martel')
+      self.assertEqual(st.exp[0]['model_training'][0]['label'], 'hell')
+      self.assertEqual(st.exp[0]['model_training'][0]['loss'], [ [] ])
+      self.assertEqual(len(st.exp[0]['model_training'][0]['time']), 1)
+      self.assertAlmostEqual(st.exp[0]['model_training'][0]['time'][0], test_start, places = 4)
+      self.assertEqual(st.exp[0]['model_training'][0]['measures'], { 'balanced_accuracy': [], 
+                                                                  'roc_area'         : [], 
+                                                                  'report'           : '' } )
+      self.assertEqual(st.exp[0]['mia_stats'], { 'mia_train_in_distribution' : [],
+                                                 'mia_train_out_distribution': [],
+                                                 'mia_test_in_distribution'  : [],
+                                                 'mia_test_out_distribution' : [] } )
         
-def add_b2(st):
-    pred = [0,0,0,0,1,0,1,1,1]
-    real = [1,0,0,0,1,0,1,0,1]
-    past_number_batch = st.batch_count[st.current_experiment_name]
-    past_y_pred = list(st.y_pred)
-    past_y_real = list(st.y_true)
-    st.add_batch_results(pred,real)
-    assert st.epoch_count[st.current_experiment_name] > 0
-    if st.epoch_count[st.current_experiment_name] == 1:
-        assert st.batch_count[st.current_experiment_name] == past_number_batch + 1
-    else:
-        assert st.batch_count[st.current_experiment_name] == past_number_batch
-    assert st.y_pred == past_y_pred + pred
-    assert st.y_true == past_y_real + real
-    #assert len(st.balanced_accuracy[st.current_experiment_name]) == (st.epoch_count[st.current_experiment_name] - 1) * st.batch_count + st.batch_count
-    #assert len(st.roc_area[st.current_experiment_name]) == (st.epoch_count - 1) * st.batch_count + st.batch_count
-    assert st.balanced_accuracy[st.current_experiment_name][-1] == 0.775
-    assert st.roc_area[st.current_experiment_name][-1] == 0.7750000000000001
+#     def test_new_add_b1(self):
+#         st = Statistics()
+#         st.new_experiment('loremipsum', {'batch_size': 64, 'lambda': 17})
+#         st.new_epoch()
+#         pred = [0,0,0,0,1]
+#         real = [1,0,0,0,1]
+#         past_number_batch = st.batch_count[st.current_experiment_name]
+#         past_y_pred = list(st.y_pred)
+#         past_y_real = list(st.y_true)
+#         st.add_batch_results(pred,real)
+#         self.assertGreater(st.epoch_count[st.current_experiment_name], 0)
+#         if st.epoch_count[st.current_experiment_name] == 1:
+#             self.assertEqual(st.batch_count[st.current_experiment_name], past_number_batch + 1)
+#         else:
+#             self.assertEqual(st.batch_count[st.current_experiment_name], past_number_batch)
+#         self.assertEqual(st.y_pred, past_y_pred + pred)
+#         self.assertEqual(st.y_true, past_y_real + real)
+#         #print(len(st.balanced_accuracy[st.current_experiment_name]), st.epoch_count[st.current_experiment_name], st.batch_count[st.current_experiment_name])
+#         #assert len(st.balanced_accuracy[st.current_experiment_name]) == (st.epoch_count[st.current_experiment_name] - 1) * st.batch_count[st.current_experiment_name] + st.batch_count[st.current_experiment_name]
+#         #assert len(st.roc_area[st.current_experiment_name]) == (st.epoch_count[st.current_experiment_name] - 1) * st.batch_count[st.current_experiment_name] + st.batch_count[st.current_experiment_name]
+#         self.assertEqual(st.balanced_accuracy[st.current_experiment_name][-1], 0.875)
+#         self.assertEqual(st.roc_area[st.current_experiment_name][-1], 0.875)
+        
+#     def test_new_exp2(self):
+#         st = Statistics()
+#         st.new_experiment('alive', {'batch_size': 64, 'beta': 17})
+#         n = '{} '.format(len(st.list_experiment) - 1) + 'alive'
+#         self.assertEqual(st.current_experiment_name, n)
+#         self.assertEqual(st.list_experiment[-1], n)
+#         self.assertEqual(st.param[n], {'batch_size': 64, 'beta': 17})
+#         self.assertEqual(st.balanced_accuracy[n], [])
+#         self.assertEqual(st.roc_area[n], [])
+#         self.assertEqual(st.batch_count[n], 0)
+#         self.assertEqual(st.epoch_count[n], 0)
+        
+#     def test_new_add_b2(self):
+#         st = Statistics()
+#         st.new_experiment('alive', {'batch_size': 64, 'beta': 17})
+#         st.new_epoch()
+#         pred = [0,0,0,0,1,0,1,1,1]
+#         real = [1,0,0,0,1,0,1,0,1]
+#         past_number_batch = st.batch_count[st.current_experiment_name]
+#         past_y_pred = list(st.y_pred)
+#         past_y_real = list(st.y_true)
+#         st.add_batch_results(pred,real)
+#         self.assertGreater(st.epoch_count[st.current_experiment_name], 0)
+#         if st.epoch_count[st.current_experiment_name] == 1:
+#             self.assertEqual(st.batch_count[st.current_experiment_name], past_number_batch + 1)
+#         else:
+#             self.assertEqual(st.batch_count[st.current_experiment_name], past_number_batch)
+#         self.assertEqual(st.y_pred, past_y_pred + pred)
+#         self.assertEqual(st.y_true, past_y_real + real)
+#         #print(len(st.balanced_accuracy[st.current_experiment_name]), st.epoch_count[st.current_experiment_name], st.batch_count[st.current_experiment_name])
+#         #assert len(st.balanced_accuracy[st.current_experiment_name]) == (st.epoch_count[st.current_experiment_name] - 1) * st.batch_count[st.current_experiment_name] + st.batch_count[st.current_experiment_name]
+#         #assert len(st.roc_area[st.current_experiment_name]) == (st.epoch_count[st.current_experiment_name] - 1) * st.batch_count[st.current_experiment_name] + st.batch_count[st.current_experiment_name]
+#         self.assertEqual(st.balanced_accuracy[st.current_experiment_name][-1], 0.775)
+#         self.assertEqual(st.roc_area[st.current_experiment_name][-1], 0.7750000000000001)
     
-def main():
-    stat = Statistics()
-    new_exp1(stat)
-    for e in range(3):
-        new_ep(stat)
-        for b in range(5):
-            add_b1(stat)
-    new_exp2(stat)
-    for e in range(1):
-        new_ep(stat)
-        for b in range(10):
-            add_b2(stat)
+# def main():
+#     stat = Statistics()
+#     new_exp1(stat)
+#     for e in range(3):
+#         new_ep(stat)
+#         for b in range(5):
+#             add_b1(stat)
+#     new_exp2(stat)
+#     for e in range(1):
+#         new_ep(stat)
+#         for b in range(10):
+#             add_b2(stat)
 
 if __name__ == '__main__':
-    #main()
     unittest.main()
